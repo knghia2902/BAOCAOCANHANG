6961baf04e4975426ac288b55b428772b9c29b81
unknown <Administrator@PCN-B760-01.hpc.intra>
2026-07-16 17:29:57 +0700

    fix: remove unused onMounted import in ToolsView.vue to resolve TS6133 build error

--- src/views/ToolsView.vue
+++ src/views/ToolsView.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref, onMounted, watch } from 'vue';
+import { ref, watch } from 'vue';
 import { useRouter } from 'vue-router';
 import { authStore } from '../stores/auth';
 import { ContentService } from '../services/ContentService';
